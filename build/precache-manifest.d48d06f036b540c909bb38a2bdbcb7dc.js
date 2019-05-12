133ea4da9be862dfd5ecee579ffde5e85a0bd406
Arif <44020819+azimarif@users.noreply.github.com>
2019-05-12 17:14:42 +0530

    Initial Commit

---
+++ build/precache-manifest.d48d06f036b540c909bb38a2bdbcb7dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd06ea440d3fc1d922c6b2ac59bcb80",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "b82b6cd5a5093c56de3a",
+    "url": "/todo/static/css/main.929dbdb8.chunk.css"
+  },
+  {
+    "revision": "8adb3f488dc349f8fffc",
+    "url": "/todo/static/js/2.59492e8c.chunk.js"
+  },
+  {
+    "revision": "b82b6cd5a5093c56de3a",
+    "url": "/todo/static/js/main.6fa9fdfa.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  }
+]);
