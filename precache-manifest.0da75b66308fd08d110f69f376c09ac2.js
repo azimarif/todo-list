d1f5626354428ac49295703eaaebeb67927f5ce0
Arif <44020819+azimarif@users.noreply.github.com>
2019-05-12 15:09:41 +0530

    Updates

---
+++ precache-manifest.0da75b66308fd08d110f69f376c09ac2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf8f418b82cce6c02bccfdd38420dcd",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "3891a30302e588ce793a",
+    "url": "/todo-list/static/css/main.3fdae2fa.chunk.css"
+  },
+  {
+    "revision": "0bd763e32748a324c0e4",
+    "url": "/todo-list/static/js/2.3cdddffd.chunk.js"
+  },
+  {
+    "revision": "3891a30302e588ce793a",
+    "url": "/todo-list/static/js/main.637568bd.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
